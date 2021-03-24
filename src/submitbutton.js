84943253402324ff5e6b2d410b74feee456b354f
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-24 20:39:17 +0530

    initializing submit button

--- src/submitbutton.js
+++ src/submitbutton.js
@@ -5,8 +5,15 @@ import './components/userstore';
 class submitbutton extends React.Component {
   render(){
     return (
-      <div className="inputform">
-       <b>Hello worldn</b> 
+      <div className="submitbutton">
+          <button
+          className='btn'
+          disabled={this.props.disabled}
+          onClick={() => this.props.onClick()}
+          >
+              {this.props.text}
+          </button>
+       
       </div>
     );
   }
