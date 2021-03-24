45a8a2acd3450b9187d1c4dc91ecc994f5c1b749
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-24 20:02:38 +0530

    function to check errors

--- src/App.js
+++ src/App.js
@@ -6,6 +6,41 @@ import loginform      from './loginform';
 import './App.css';
 
 class App extends React.Component{
+
+  // API part
+  async componentDidMount() {
+    try{
+      let res = await fetch('/isLoggedIn',{
+        method : 'post',
+        header :{
+          'Accept' : 'application/json',
+          'content-type' : 'application/json',
+        }
+      });
+
+      let result = await res.json()
+
+      //checking whether user is registered
+      if(result && result.success)
+      {
+        userstore.loading = 'false';
+        userstore.isLoggedIn = 'true';
+        userstore.username = result.username;
+      }
+
+      else{
+        userstore.loading = 'false';
+        userstore.isLoggedIn = 'false';
+      }
+    }
+    catch(error)
+    {
+      userstore.loading = 'false';
+      userstore.isLoggedIn = 'false';
+    }
+  }
+
+  
   render(){
     return (
       <div className="App">
